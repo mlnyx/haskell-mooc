6b6f0f4c6d771366cf18cd02198fb97cae08e461
노승민 <nsm110@naver.com>
2023-10-17 01:28:25 +0900

    Ex5: Solved the 5th  exercise.

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -69,7 +69,7 @@ distance x1 y1 x2 y2 = sqrt ((x1 - x2)**2 + (y1 - y2)**2)
 -- Ps. have a look at the built in function "even"
 
 eeny :: Integer -> String
-eeny = todo
+eeny n = if even n then "eeny" else "meeny"
 
 ------------------------------------------------------------------------------
 -- Ex 6: here's the function checkPassword from the course material.
